f14f80c0345e8411c196f21321d44ecff4f0d91f
juanjmg <juanjosemorales1986@gmail.com>
2023-03-02 18:03:45 -0300

    prototipo segunda preentrega

--- main.js
+++ main.js
@@ -1,140 +1,100 @@
-//SOCIOS:
-
-//Funcion para crear los productos con sus precios
-function productos(tipo){
-    let precio;
-    switch(tipo){
-        case 1:
-            precio = 15;
-            break
-        case 2:
-            precio = 21
-            break
-        case 3:
-            precio = 80
-            break
-        case 4:
-            precio = 99
-            break
-        case 5:
-            precio = 10
-            break
-        case 6:
-            precio = 15
-            break
-        case 7:
-            precio = 3
-            break
-        case 8:
-            precio = 5
-            break
-    }
-    return precio;
+//!Segunda Entrega --- Juego Frio  --- 
+console.log(`Inicia tu viaje hacia un campamento con temperaturas invernales de -20 grados y bajando a maximas de -90 grados bajo cero,  dependiendo de tu ruta se emplearan mas objetos o menos objetos segun la seleccion.
+Necesitaras llegar hasta el campamento para averiguar si el ultimo bastion de la humanidad sigue en pie en este mundo que se a convertido en un congelador gigantesco`)
 
 
-    
-}
+console.log(`Tienes Adelante un viaje con tres rutas diferentes cada ruta representa un desgaste en tu inventario y en tu salud total, a lo largo del viaje podras encontrar objetos que te ayudaran a sobrevivir hasta el final`)
 
-showstock()
+let Life = 100
+let Endurace = 500
+let Keepgoing = true
 
-function showstock(){//una mejora de los alerts que tenia
-    const producto = Number(prompt(
-`Los combos del dia son: 
-1) combo1: pochoclos + gaseosa. 
-2) combo2: pochoclos + gaseosa + chocolate. 
-3) combo3: 2x pochoclos + 2x gaseosa + chocolate + gomitas.
-4) combo4: 3x pochoclos + 3x gaseosa + 3x chocolate + 3x gomitas.
 
-Nota: "Ingresar SOLO numeros dle 1 al 3"
-`));
-
-        const producto2 = Number(prompt(
-        `Por unidad solo: 
-        5) gomitas 
-        6) birra
-        7) caramelos
-        8) chicles
-        Nota: "Ingresar SOLO numeros dle 4 al 7"`))
+class Backpack{
+    constructor(food, wood, compass, pedernal, yesca, water, campingtent, gasstove, knive, shovel) {
+        this.food = food;
+        this.compass   = compass;
+        this.wood  = wood;
+        this.perdernal  = pedernal;
+        this.yesca  = yesca;
+        this.water  = water;
+        this.campingtent  = campingtent;
+        this.gasstove  = gasstove;
+        this.knive  = knive;
+        this.shovel  = shovel;
+        this.check = function(){console.log(`En tu mochila cargas estos Items: Unidades
+        Alimento: ${this.food} Unidades
+        Burjula: ${this.compass} Unidades
+        Madera: ${this.wood} Unidades
+        Pedernal: ${this.pedernal} Unidades
+        Yesca: ${this.yesca} Unidades
+        Agua Embotellada: ${this.water} Unidades
+        Tienda de Camping: ${this.campingtent} Unidades
+        Estuda a Gas: ${this.gasstove} Unidades
+        Cuchillo Afilado: ${this.knive} Unidades
+        Pala: ${this.shovel} Unidades
+        `)}
         
-
-        const socio = String(prompt(`Es socio de la empresa?,Responda solo con "SI" O "NO"`)).toUpperCase()
-
-    productprocess(producto,producto2,socio)//llamo la funcion productprocess para correrla ya que tengo las dos variables llenas
-    // console.log(producto);//prueba
-    // console.log(producto2);//prueba    
-}
-
-
-function productprocess(producto,producto2,socio){
-    //Compruebo si escoge algo o no
-if (((producto != 0)) || ((producto2 !=0)) || ((socio !=""))) {
-    console.log(`Su seleccion es: Combo: ${producto} Unidad: ${producto2}`+ ` 🔶🍿🍭🎦🔶`);
-    const val = productos(producto)
-    const val2 = productos(producto2)
-   
-    let total = calculo(val,val2)
-    
-    let porc5 = desc5(total)
-    
-    let usuario = afiliado(socio,total,porc5)
-    
-    const desicion = function (a,b) {if (a < b){return a}else{return b}}
-    let resultado = Number(desicion(usuario,total))
-    
-   
-    alert("El monto a abonar de sus productos es de: "+resultado+" USD")
-
-}else{
-    alert(`No escogio nada. Se reiniciara la operacion`);
-    showstock();
-    
-}
-}
-//Sumo los objetos escogidos para trabajar despues con los descuentos en los condionales
-function calculo(val,val2){
-    let suma = val + val2
-    return suma
-}
-//Saco el resto del descuento a partir del total y lo guardo
-function desc5(total){
-const porcentaje = function (a,b,c) {return (a / b)*c}
-let resto = Number(porcentaje(5,100,total))
-return resto//se debe colocar return
-}
-
-//Comparo si es socio y lo que gasto
-function afiliado(socio,total,porc5){
-if(socio === "SI"){
-    if((total <= 50) && (total >= 5)){
-        totaldesc = (total - 1)
-        return totaldesc
-    }else if((total >= 50) && (total < 100)){
-        totaldesc = (total - porc5);
-        return totaldesc
-    }else if(total >= 100){
-        totaldesc = (total - 15)
-        return totaldesc
-    
-}else{
-        return total
     }
-    
-}else{
-    alert(`no es socio "PERO ESPERE 😁"`)
-    let calculo = nosocio(total)
-    return calculo
-}
 }
 
-//Funcion si no es socio
-function nosocio(total){
-    if (total>100){
-        totaldesc =  (total - 5)  
-        return totaldesc
-    }else if (total<100){
-        return total 
+
+function settlementsA(position){
+let foodconsumption
+let woodconsumption
+let yescaconsumption
+let waterconsumption
+let enduranceconsumption
+switch(position){
+    case 1:
+    foodconsumption = 20
+    woodconsumption = 15
+    yescaconsumption = 10
+    waterconsumption = 20
+    enduranceconsumption = 100
+    break
+}
+return foodconsumption, 
+            woodconsumption, 
+                yescaconsumption,
+                    waterconsumption,
+                        enduranceconsumption
+
+}
+
+
+const Juagador1 = new Backpack(20, 40, 1, 1, 50, 20, 1, 1, 1, 1);
+
+Juagador1.check();
+
+function journeyManagement(settlementsA,Jugador1){
+    return this.food = Jugador1.food - settlementsA(foodconsumption)
+}
+
+questions1();
+
+function questions1(){
+const desition = String(prompt(`Elija la ruta que desea tomar:
+Ruta A
+Ruta B
+Ruta C
+Nota:"Ingrese solo las letras A,B O C"
+`).toUpperCase());
+
+//console.log(desition);
+adventure(desition)
+
+
+}
+
+function adventure(Juagador1,desition){
+  
+    if(desition == "A" ){
+        console.log(desition);
+        console.log(`Tu viaje sale de la base hacia el yermo Super Frio.Llegaras el primer asentamiento hacia la ruta ${desition}`)
     }else{
-    alert('Verifique el tipo de socio y los productos. Vuelva a correr el programa, presione "/ESC/"'); 
-    
-    }
+    console.log(`no ingreso nada`)
+
+
+}
 }
